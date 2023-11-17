809158fe86144d35b8f2bd2e5a8b53b78c8915e9
Robert Schnüll <mail@robertschnuell.de>
2023-11-17 16:04:41 +0100

    Updated users.js to handle 'x-spacedeck-api-token' in request headers and set it as 'sdsession' cookie

--- routes/api/users.js
+++ routes/api/users.js
@@ -26,7 +26,8 @@ router.get('/current', function(req, res, next) {
     delete u.confirmation_token;
     u.token = req.cookies['sdsession'];
 
-    console.log(u);
+    // If the request headers contain "x-spacedeck-api-token", set a cookie named 'sdsession' with the value of "x-spacedeck-api-token".
+    if(req.headers["x-spacedeck-api-token"]) res.cookie('sdsession', req.headers["x-spacedeck-api-token"], { domain: domain, httpOnly: true });
     
     res.status(200).json(u);
   } else {
